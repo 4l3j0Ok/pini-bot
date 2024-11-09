69ebd188b8791dcb12858b146f406ec9953355f8
4l3j0Ok <alejofsarmiento@gmail.com>
2024-11-09 15:40:01 -0300

    fix: get app_path with os module

--- src/config.py
+++ src/config.py
@@ -8,6 +8,7 @@ LOGGER_CHANNEL_ID = int(os.environ.get("LOGGER_CHANNEL_ID"))
 ENABLED_PLUGINS = os.environ.get("ENABLED_PLUGINS", "").split(",")
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
 TESTING = os.environ.get("TESTING") == "True"
+APP_PATH = os.path.dirname(os.path.abspath(__file__))
 
 # Challenges plugin
 WEEKLY_CHALLENGES_CHANNEL_ID = int(os.environ.get("WEEKLY_CHALLENGES_CHANNEL_ID"))
@@ -15,9 +16,9 @@ WEEKLY_CHALLENGE_DAYS = [
     int(day) for day in os.getenv("WEEKLY_CHALLENGE_DAYS", "0,1,2,3,4,5,6").split(",")
 ]
 WEEKLY_CHALLENGE_HOUR = int(os.getenv("WEEKLY_CHALLENGE_HOUR", "12"))
-FIND_CHALLENGES_PATH = "/app/challenges/new"
-PROCESSED_CHALLENGES_PATH = "/app/challenges/processed"
-INITIAL_CHALLENGES_PATH = "/app/src/plugins/challenges.md"
+FIND_CHALLENGES_PATH = f"{APP_PATH}/../challenges/new"
+PROCESSED_CHALLENGES_PATH = f"{APP_PATH}/../challenges/processed"
+INITIAL_CHALLENGES_PATH = f"{APP_PATH}/plugins/challenges.md"
 MONGO_HOST = os.getenv("MONGO_HOST")
 MONGO_PORT = int(os.getenv("MONGO_PORT"))
 MONGO_USER = os.getenv("MONGO_USER")
